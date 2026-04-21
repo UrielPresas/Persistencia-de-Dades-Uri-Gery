61371357295034fa3ce3e4e759a9528813a2aaca
Gerard Martínez Gonzalez <g.martinez4@sapalomera.cat>
2026-04-21 13:38:27 +0200

    connexio ok, mostra dades

---
+++ src/DAO/Connexions/ConnexioMySQL.java
@@ -0,0 +1,33 @@
+package DAO.Connexions;
+import java.sql.*;
+
+
+public class ConnexioMySQL {
+
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://127.0.0.1:3306/Pillam";
+        String user = "root";
+        String pass = "Blanes2007!";
+        String sql = "SELECT * FROM sector";
+
+
+        try (
+                Connection con = DriverManager.getConnection(url, user, pass);
+                PreparedStatement ps = con.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+        ) {
+
+            while (rs.next()) {
+                System.out.println(
+                        rs.getInt("id_escalador") + " | " +
+                                rs.getString("nom") + " | " +
+                                rs.getString("alias") + " | " +
+                                rs.getInt("edat") + " | " +
+                                rs.getString("nivell_maxim") + " | " +
+                                rs.getString("estil_preferit")  );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+}
+}
