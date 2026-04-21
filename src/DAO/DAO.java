a8800261fda3a6887e894b6be4b4690dcac98c00
UrielPresas <u.presas@sapalomera.cat>
2026-04-21 13:32:52 +0200

    Comienzo de DAO, y de model(Escalador,Escola,Llarg,Sector)

---
+++ src/DAO/DAO.java
@@ -0,0 +1,13 @@
+package DAO;
+
+import java.util.List;
+
+// T == Tipus d'objecte (Candidat, Via, Sector...)
+// K == Tipus de clau primaria (Long, String, int, Float...)
+public interface MySQLDAO <T, K>{
+    void inserir(T c);
+    void modificar(T c);
+    void eliminar(T c);
+    List<T> obtindreTots();
+    T obtenir(K id);
+}
